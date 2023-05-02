15ae30ea370dcedbaf41b8a8a9bdf3c33d556a33
cxzaqq <roy1228@naver.com>
2023-05-02 21:11:15 +0900

    백엔드 로그인 기능 구현

--- login-lecture/app/src/public/js/home/login.js
+++ login-lecture/app/src/public/js/home/login.js
@@ -1,4 +1,24 @@
 "use strict";
 
-console.log("hello");
-console.log("hi");
+const id = document.querySelector("#id"),
+  pw = document.querySelector("#pw"),
+  loginBtn = document.querySelector("button");
+
+const login = () => {
+  const req = {
+    id: id.value,
+    pw: pw.value,
+  };
+
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  })
+    .then((res) => res.json())
+    .then(console.log);
+};
+
+loginBtn.addEventListener("click", login);
--- login-lecture/app/src/routes/home/home.ctrl.js
+++ login-lecture/app/src/routes/home/home.ctrl.js
@@ -1,14 +1,40 @@
 "use strict";
 
-const home = (req, res) => {
-  res.render("home/index");
+const users = {
+  id: ["11", "22", "aa"],
+  pw: ["11", "22", "bb"],
 };
 
-const login = (req, res) => {
-  res.render("home/login");
+const output = {
+  home: (req, res) => {
+    res.render("home/index");
+  },
+  login: (req, res) => {
+    res.render("home/login");
+  },
+};
+
+const process = {
+  login: (req, res) => {
+    const id = req.body.id,
+      pw = req.body.pw;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.pw[idx] === pw) {
+        return res.json({
+          sucess: true,
+        });
+      }
+    }
+    return res.json({
+      success: false,
+      msg: "login failed",
+    });
+  },
 };
 
 module.exports = {
-  home,
-  login,
+  output,
+  process,
 };
