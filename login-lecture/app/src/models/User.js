1c50c86165c14cffe2b481285d33e45767d58eb5
cxzaqq <roy1228@naver.com>
2023-05-05 16:53:08 +0900

    회원가입 임시 구현

--- login-lecture/app/src/models/User.js
+++ login-lecture/app/src/models/User.js
@@ -8,15 +8,21 @@ class User {
   }
 
   login() {
-    const body = this.body;
-    const { id, pw } = UserStorage.getUserInfo(body.id);
+    const client = this.body;
+    const { id, pw } = UserStorage.getUserInfo(client.id);
     if (id) {
-      if (id === body.id && pw === body.pw) {
+      if (id === client.id && pw === client.pw) {
         return { success: true };
       }
       return { success: false, msg: "wrong password" };
     }
     return { success: false, msg: "id does not exist" };
+  }
+
+  register() {
+    const client = this.body;
+    const response = UserStorage.save(client);
+    return response;
   }
 }
 module.exports = User;
---
+++ login-lecture/app/src/public/js/home/register.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const id = document.querySelector("#id"),
+  name = document.querySelector("#name"),
+  pw = document.querySelector("#pw"),
+  confirmPw = document.querySelector("#confirm-pw"),
+  registerBtn = document.querySelector("#button");
+
+const register = () => {
+  if (!id.value) return alert("input Id");
+  if (!pw.value) return alert("input password");
+  if (pw.value !== confirmPw.value) return alert("wrong confrim password");
+  const req = {
+    id: id.value,
+    name: name.value,
+    pw: pw.value,
+  };
+
+  fetch("/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        location.href = "/login";
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error("register error");
+    });
+};
+
+registerBtn.addEventListener("click", register);
